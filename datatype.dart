3dd60b15d93dd78cde052642e9f580235166b159
sreedevbiju1-design <sreedevbiju1@gmail.com>
2026-08-05 14:08:42 +0530

    Add files via upload

---
+++ datatype.dart
@@ -0,0 +1,40 @@
+void main() {
+  // int age=21;
+  // print(age);
+  //
+  //  double height=5.8;
+  // print(height);
+  //
+  // num a=12;
+  // num b=23;
+  // print(a);
+  //
+  // String name='sreedev';
+  // print(name.length);
+  //
+  // String h='Hello';
+  // String z='world';
+  // print(h+'  '+z);
+  //
+  // String A='HeLLo';
+  // print(A.toUpperCase());
+  // print(A.toLowerCase());
+  //
+  // List<String> fruits=['apple','banana','mango'];
+  // print(fruits[1]);
+  // fruits.add('cherry');
+  // print(fruits);
+  // fruits.insert(2,'kiwi');
+  // print(fruits);
+  // fruits.remove('apple');
+  // print(fruits);
+  // print(fruits.length);
+
+  Set<String> colors = {'red', 'blue', 'green'};
+  print(colors);
+
+  Map<String,String> stud={'name':'harii','course':'bca','city':'kannur'};
+  print(stud['name']);
+  stud['phone']='9037181319';
+  print(stud);
+}
---
+++ practical_qs.dart
@@ -0,0 +1,79 @@
+void main(){
+  // String studentname='arjun';
+  // int age=21;
+  // int percentage=75;
+  // bool passed=true;
+  // print(studentname);
+  // print(age);
+  // print(percentage);
+  // print(passed);
+
+  // String Employeename='vishnu';
+  // int Employeeid=6974;
+  // int salary=65000;
+  // bool ispermanentemployee=true;
+  // print(Employeename);
+  // print(Employeeid);
+  // print(salary);
+  // print(ispermanentemployee);
+
+  // String productname='Oreo';
+  // int productprice=40;
+  // int productquantity=5;
+  // bool productavailable=false;
+  //
+  // print(productname);
+  // print(productprice);
+  // print(productquantity);
+  // print(productavailable);
+
+  // String name='sreedev';
+  // String city='kannur';
+  // int mobile=9037181319;
+  // int height=159;
+  // int weight=61;
+  //
+  // print(name);
+  // print(city);
+  // print(mobile);
+  // print(height);
+  // print(weight);
+
+  // String moviename='abc';
+  // String heroname='dulqar';
+  // int rating=6;
+  // bool released=true;
+  // print(moviename);
+  // print(heroname);
+  // print(rating);
+  // print(released);
+
+  // int phy=37;
+  // int chem=39;
+  // int bio=43;
+  // int maths=38;
+  // int mal=46;
+  // print(phy+chem+bio+maths+mal);
+  // int total=phy+chem+bio+maths+mal;
+  // double avg=total/5;
+  // print(avg);
+
+  // int weight=67;
+  // int height=160;
+  // double BMI=(weight/height*height);
+  // print(BMI);
+  //
+// String firstname='john';
+// String lastname='doe';
+// String fullname=(firstname+lastname);
+// print(fullname);
+// String housename='Meyana';
+// String city='vadakara';
+// String state='kerela';
+// String country='india';
+// print('house name is'+' '+housename+'\n'+'city is'+' '+city+'\n'+'stste is'+' '+state+'\n'+'country is'+' '+country+'\n');
+
+String firstname='john';
+String lastname='doe';
+print(firstname+'.'+lastname+'@gmail.com');
+}
