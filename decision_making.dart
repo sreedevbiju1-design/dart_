f1a86bb6f2b7e1e62793da634d2b035739500091
sreedevbiju1-design <sreedevbiju1@gmail.com>
2026-08-07 14:53:18 +0530

    Add files via upload

---
+++ decision_making.dart
@@ -0,0 +1,96 @@
+void main(){
+  // int a=5;
+  // int b=15;
+  // if(a>b) {
+  //   print('a is greatest');
+  // }
+  // else{
+  //   print('b is greater');
+  // }
+
+  // int c=2;
+  // if(c>0){
+  //   print('c is positive');
+  // }
+
+  // int a=20;
+  // if(a>18){
+  //   print('eligible to vote');
+  // }
+  // int a=29;
+  // if(a%5==0){
+  //   print('divisible by 5');
+  // }
+  // else{
+  //   print('not divisible by 5');
+  // }
+
+  // int a=11;
+  // if(a%2==0){
+  //   print('even number');
+  // }
+  // else{
+  //   print('not an aven number');
+  // }
+
+  // int m=45;
+  // if(m>=40) {
+  //   print('pass');
+  // }
+  //   else{
+  //     print('fail');
+  // }
+
+  // int a=16;
+  // if(a>=18){
+  //   print('adult');
+  // }
+  // else{
+  //   print('minor');
+  // }
+
+  // int a=0;
+  // if(a>0){
+  //   print('positive');
+  // }
+  // else{
+  //   print('negative');
+
+  // int a=84;
+  // if(a>=90&&a<=100){
+  //   print('A');
+  // }
+  // if(a>=80&&a<=89){
+  //   print('b');
+  // }
+  // if(a>=70&&a<=79){
+  //   print('c');
+  // }
+  // if(a>=60&&a<=69){
+  //   print('d');
+  // }
+  // if(a<60){
+  //   print('f');
+  // }
+  int a=10;
+  int b=35;
+  int c=22;
+  if(a>b){
+    print('a is largest');
+  }
+  if(a>c){
+    print('a is largest');
+  }
+  if(b>a){
+    print('b is largest');
+  }
+  if(b>c){
+    print('b is largest');
+  }
+  if(c>a){
+    print('c is largest');
+  }
+  if(c>b){
+    print('c is largest');
+  }
+}
